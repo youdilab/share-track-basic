2116d544cbf4a4feaf615c57ec4d7f3cb43e6247
youdilab <youdilabnotes@gmail.com>
2024-05-05 21:32:40 +0530

    Added code to read and update parameter values.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -136,6 +136,8 @@ void YoudiShareAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
+    //juce::Logger::writeToLog("Youdi");
+
     // In case we have more outputs than inputs, this code clears any output
     // channels that didn't contain input data, (because these aren't
     // guaranteed to be empty - they may contain garbage).
